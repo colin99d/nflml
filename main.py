3b2253b2cab622c44f670a23c82fc762bdf0425a
colin99d <colin99delahunty@gmail.com>
2022-07-16 22:40:56 -0400

    Switched to class based system

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Any
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model
 import pandas as pd
@@ -5,65 +6,120 @@ import numpy as np
 import typer
 
 
-def k_nearest(df: pd.DataFrame):
-    """Predicts whether a team will win based on past performance"""
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    df["won"] = np.where(df["won_by"] > 0, 1, 0)
-    neigh.fit(df[["avg_pass_yds_y", "avg_rush_yds_y"]], df["won"])
-    print(neigh.score(df[["avg_pass_yds_y", "avg_rush_yds_y"]], df["won"]))
+offense_unused = [
+    "Surface",
+    "Temperature",
+    "Humidity",
+    "Wind_Speed",
+    "Roof",
+    "OT",
+    "Team_abbrev",
+    "Opponent_abbrev",
+    "game_date",
+    "player_id",
+    "player",
+]
 
 
-def linear_regression(df: pd.DataFrame):
-    """Predicts how much the home team will win by"""
-    reg = linear_model.LinearRegression()
-    reg.fit(df[["avg_pass_yds_y", "avg_rush_yds_y"]], df["won_by"])
-    print(reg.score(df[["avg_pass_yds_y", "avg_rush_yds_y"]], df["won_by"]))
+defense_unused: List[str] = []
+kicker_unused: List[str] = []
 
 
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.loc[df["pos"] == "QB"]
-    df = df.drop(
-        ["Surface", "Temperature", "Humidity", "Wind_Speed", "Roof", "OT"],
-        axis=1,
-    )
-    df["is_home"] = df["team"] == df["home_team"]
-    df["won_by"] = np.where(
-        df["is_home"],
-        df["home_score"] - df["vis_score"],
-        df["vis_score"] - df["home_score"],
-    )
-    # Account for multiple quarterbacks in a game by adding their performances
-    analyze_cols = ["pass_yds", "rush_yds"]
-    for col in analyze_cols:
-        df[f"tot_{col}"] = df.groupby(["game_id", "team"])[col].transform("sum")
+class Data:
+    unused = {
+        "offense": offense_unused,
+        "defense": defense_unused,
+        "kicking": kicker_unused,
+    }
+
+    def __init__(self, analyze):
+        self.analyze = analyze
+        self.offense = pd.DataFrame()
+        self.defense = pd.DataFrame()
+        self.kicking = pd.DataFrame()
+
+        for item in ["offense", "defense", "kicking"]:
+            df = pd.read_csv(f"{item}.csv")
+            df = df.drop(self.unused[item], axis=1)
+            if item == "offense":
+                df = df[df["pos"] == "QB"]
+            setattr(self, item, df)
+        for item in self.analyze["offense"]:
+            self.add_rolling(self.offense, item)
+        self.clean_offense()
+
+    def clean_offense(self):
+        df = self.offense
+        for column in self.analyze["offense"]:
+            df = df[df[f"avg_{column}"].notna()]
+        df = df.drop(["pos"], axis=1)
+        df_home = df[df["team"] == df["home_team"]]
+        df_away = df[df["team"] != df["home_team"]]
+        df_away = df_away.drop(
+            [
+                "Vegas_Line",
+                "Vegas_Favorite",
+                "Over_Under",
+                "vis_team",
+                "home_team",
+                "vis_score",
+                "home_score",
+            ],
+            axis=1,
+        )
+        combined = df_home.merge(df_away, on="game_id", suffixes=["_home", "_away"])
+        combined["net_score"] = combined["home_score"] - combined["vis_score"]
+        for item in self.analyze["offense"]:
+            combined[f"net_avg_{item}"] = (
+                combined[f"avg_{item}_home"] - combined[f"avg_{item}_away"]
+            )
+        self.offense = combined
+
+    def add_rolling(self, df: pd.DataFrame, column: str, rolling: int = 10):
+        """Creates rolling avergae for given column. Does NOT include most recent row
+        because we do not have this data beforehand"""
+        tmp = df.copy(deep=True)
+        tmp[f"tot_{column}"] = tmp.groupby(["game_id", "team"])[column].transform("sum")
         avg_row = (
-            df.groupby("team")[f"tot_{col}"]
-            .rolling(10, closed="left", min_periods=1)
+            tmp.groupby("team")[f"tot_{column}"]
+            .rolling(rolling, closed="left", min_periods=1)
             .mean()
         )
-        avg_row = avg_row.rename(f"avg_{col}")
+        avg_row = avg_row.rename(f"avg_{column}")
         avg_row.index = [x[1] for x in avg_row.index]
-        df = df.merge(avg_row, left_index=True, right_index=True)
-    df.drop_duplicates(["game_id", "team"], inplace=True)
-    for col in analyze_cols:
-        df = df[df[f"avg_{col}"].notna()]
-    for col in analyze_cols:
-        df[f"avg_{col}"] = np.where(df["is_home"], df[f"avg_{col}"], -df[f"avg_{col}"])
-    cols = [f"avg_{col}" for col in analyze_cols]
-    temp_df = df[["game_id"] + cols].groupby("game_id").agg("sum")
-    df = df.merge(temp_df, on="game_id")
-    df = df.loc[df["is_home"] == True]
+        tmp = tmp.merge(avg_row, left_index=True, right_index=True)
+        tmp.drop_duplicates(["game_id", "team"], inplace=True)
+        self.offense = tmp
 
-    return df
+
+class Analyze:
+    def __init__(self, analyze: Dict[str, Any], offense: pd.DataFrame):
+        self.analyze = analyze
+        self.offense = offense
+        self.k_nearest(self.offense)
+        self.linear_regression(self.offense)
+
+    def k_nearest(self, df: pd.DataFrame):
+        """Predicts whether a team will win based on past performance"""
+        neigh = KNeighborsClassifier(n_neighbors=3)
+        columns = [f"net_avg_{x}" for x in self.analyze["offense"]]
+        neigh.fit(df[columns], df["net_score"])
+        print(neigh.score(df[columns], df["net_score"]))
+
+    def linear_regression(self, df: pd.DataFrame):
+        """Predicts how much the home team will win by"""
+        reg = linear_model.LinearRegression()
+        columns = [f"net_avg_{x}" for x in self.analyze["offense"]]
+        reg.fit(df[columns], df["net_score"])
+        print(reg.score(df[columns], df["net_score"]))
 
 
 def main(save: bool = False):
-    df = pd.read_csv("data.csv")
-    nfl = clean_df(df)
-    linear_regression(nfl)
-    k_nearest(nfl)
+    analyze = {"offense": ["pass_yds", "rush_yds"], "defense": [], "kicking": []}
+    data = Data(analyze)
+    analyze = Analyze(analyze, data.offense)
     if save:
-        nfl.to_csv("export.csv")
+        data.offense.to_csv("export.csv")
 
 
 if __name__ == "__main__":
